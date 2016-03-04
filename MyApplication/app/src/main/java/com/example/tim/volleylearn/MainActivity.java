6b8e4d693b5fbf1d355fd308ceb8bab91974337b
sungen <sungentim@gmail.com>
2016-03-04 16:01:00 +0800

    学习使用Volley

--- MyApplication/app/src/main/java/com/example/tim/volleylearn/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/tim/volleylearn/MainActivity.java
@@ -2,6 +2,13 @@ package com.example.tim.volleylearn;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sendHttpRequest();
+    }
+
+    private void sendHttpRequest() {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        StringRequest stringRequest = new StringRequest("http://www.baidu.com", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d("TAG", response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("TAG",  error.getMessage(), error);
+            }
+        });
+        queue.add(stringRequest);
     }
 }
