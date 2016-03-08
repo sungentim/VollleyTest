cc5a617e0672ed0a6798a03c84f68550259e3d21
sungen <sungentim@gmail.com>
2016-03-08 17:29:09 +0800

    学习使用Volley

--- MyApplication/app/src/main/java/com/example/tim/volleylearn/RecyclerViewDemo.java
+++ MyApplication/app/src/main/java/com/example/tim/volleylearn/RecyclerViewDemo.java
@@ -4,16 +4,22 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.OrientationHelper;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example.tim.volleylearn.recyclerview.DividerGridItemDecoration;
+import com.example.tim.volleylearn.recyclerview.DivierItemDecoration;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -36,18 +42,19 @@ public class RecyclerViewDemo extends AppCompatActivity {
 
     private void initData() {
         mDatas = new ArrayList<String>();
-        for (int i = 0; i < 40; i++) {
+        for (int i = 0; i < 80; i++) {
             mDatas.add("item" + i);
         }
     }
 
     private void initView() {
         adapter = new MyRecycelerAdapter(this,mDatas);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        StaggeredGridLayoutManager layoutManager = new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL);
         mRecyclerView.setLayoutManager(layoutManager);
-        layoutManager.setOrientation(OrientationHelper.HORIZONTAL);
+//        layoutManager.setOrientation(OrientationHelper.VERTICAL);
         mRecyclerView.setAdapter(adapter);
         mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        mRecyclerView.addItemDecoration(new DividerGridItemDecoration(this));
 
 
     }
@@ -57,16 +64,24 @@ public class RecyclerViewDemo extends AppCompatActivity {
         private Context mContext;
         private LayoutInflater inflater;
 
+        private List<Integer> mHeights ;
         public MyRecycelerAdapter(Context context, List<String> datas){
             this. mContext=context;
             this. mDatas=datas;
             inflater=LayoutInflater. from(mContext);
+            mHeights = new ArrayList<>();
+            for(String item:datas){
+                mHeights.add((int) (100+Math.random()*300));
+            }
         }
 
         @Override
         public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
             View view = inflater.inflate(R.layout.simple_recycler_item,parent,false);
+
+
+
 
             MyViewHolder viewHolder = new MyViewHolder(view);
 
@@ -76,6 +91,13 @@ public class RecyclerViewDemo extends AppCompatActivity {
         //填充onCreateViewHolder返回的holder控件
         @Override
         public void onBindViewHolder(MyViewHolder holder, int position) {
+
+            ViewGroup.LayoutParams params = holder.tv.getLayoutParams();
+
+            params.height = mHeights.get(position);
+
+            holder.tv.setLayoutParams(params);
+
             holder.tv.setText(mDatas.get(position));
         }
 
